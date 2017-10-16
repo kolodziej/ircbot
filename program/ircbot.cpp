56e63a7a5c23de409408a155b0dad18ca70fff52
Kacper Kołodziej <kacper@kolodziej.in>
2017-10-16 23:20:49 +0200

    Add support for cli options in program/ircbot

--- program/ircbot.cpp
+++ program/ircbot.cpp
@@ -15,11 +15,41 @@ namespace asio = boost::asio;
 namespace opt = boost::program_options;
 
 int main(int argc, char **argv) {
+  std::string config_fname;
   
-  opt::options_description opts("IRCBot client");
-  opts.add_options()
-    ("h,help", "show help message")
+  opt::options_description cmd_opts("IRCBot client");
+  cmd_opts.add_options()
+    ("help,h", "show help message")
+    ("config,c",
+       opt::value<std::string>(&config_fname)->required(),
+       "configuration file")
   ;
+
+  opt::variables_map var_map;
+
+
+  try {
+    try {
+      opt::store(opt::parse_command_line(argc, argv, cmd_opts), var_map);
+
+      if (var_map.count("help")) {
+        std::clog << cmd_opts;
+        return 0;
+      }
+
+      opt::notify(var_map);
+    } catch (opt::error& exc) {
+      std::clog << exc.what() << '\n';
+      std::clog << cmd_opts;
+      return 1;
+    }
+
+  } catch (std::exception& exc) {
+    std::clog << "Terminating application due to unhandled exception. "
+        << "Check logs for details.\n";
+    LOG(ERROR, "Unhandled exception: ", exc.what(), ". Terminating.");
+    return 2;
+  }
 
   return 0;
 }
