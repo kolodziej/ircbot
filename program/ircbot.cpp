ca7da3f3c686a56f4e587c6c8fad36738dc209a9
Kacper Kołodziej <kacper@kolodziej.in>
2017-10-24 22:21:27 +0200

    Run Client instance in IRCBot; add daemon mode

--- program/ircbot.cpp
+++ program/ircbot.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <thread>
 
+#include <signal.h>
+
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 
@@ -17,14 +19,18 @@ namespace opt = boost::program_options;
 
 int main(int argc, char **argv) {
   std::string config_fname;
+  bool daemon;
   
   opt::options_description cmd_opts("IRCBot client");
   cmd_opts.add_options()
     ("help,h", "show help message")
     ("version,v", "show version information")
+    ("daemon,d",
+     opt::bool_switch(&daemon)->default_value(false),
+     "run as a daemon")
     ("config,c",
-       opt::value<std::string>(&config_fname)->required(),
-       "configuration file")
+     opt::value<std::string>(&config_fname)->required(),
+     "configuration file")
   ;
 
   opt::variables_map var_map;
@@ -49,6 +55,33 @@ int main(int argc, char **argv) {
       std::clog << cmd_opts;
       return 1;
     }
+
+    Logger& logger = Logger::getInstance();
+    if (not daemon) {
+      logger.addOutput(LogOutput{std::cerr, LogLevel::INFO});
+    }
+
+    if (daemon) {
+      pid_t id = fork();
+      if (id == -1) {
+        LOG(ERROR, "Error occurred during forking daemon process. Continuing in foreground!");
+      } else if (id > 0) {
+        LOG(INFO, "Succesfully forked daemon process (PID ", id, ") to background!");
+        return 0;
+      }
+    }
+
+    asio::io_service io;
+    Config config(config_fname);
+    Client client(io, config);
+
+
+    client.pluginManager().startPlugins();
+
+    std::thread io_thread([&io] { io.run(); });
+    client.spawn();
+
+    io_thread.join();
 
   } catch (std::exception& exc) {
     std::clog << "Terminating application due to unhandled exception. "
