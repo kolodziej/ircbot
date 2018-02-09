43d2731f5722284701eefb8688e72f7c89ee1350
Kacper Kołodziej <kacper@kolodziej.it>
2018-02-09 01:35:50 +0100

    Add skeleton of CommandPlugin

--- include/ircbot/command_plugin.hpp
+++ include/ircbot/command_plugin.hpp
@@ -3,13 +3,30 @@
 
 #include "ircbot/plugin.hpp"
 
+#include <string>
+#include <vector>
+#include <functional>
+#include <unordered_map>
+
+#include "ircbot/client.hpp"
+#include "ircbot/command_parser.hpp"
+
 class CommandPlugin : public Plugin {
  public:
-  CommandPlugin();
+  using CmdFunction = std::function<void(const CommandParser::Command&)>;
+  CommandPlugin(Client& client, std::string name, char prefix);
 
-  virtual void run() override;
-  virtual void onMessage() override;
+  virtual void onMessage(IRCCommand cmd) override;
   virtual bool filter(const IRCCommand& cmd) override;
+
+ private:
+  void addCommand(const std::string& cmd, CmdFunction);
+  void removeCommand(const std::string& cmd);
+  void callCommand(const CommandParser::Command& command);
+  void runParsedCommands();
+
+  CommandParser m_parser;
+  std::unordered_map<std::string, CmdFunction> m_functions;
 };
 
 #endif
---
+++ source/command_plugin.cpp
@@ -0,0 +1,57 @@
+#include "ircbot/command_plugin.hpp"
+
+CommandPlugin::CommandPlugin(Client& client,
+                             std::string name,
+                             char prefix) :
+    Plugin{client, name},
+    m_parser{ParserConfig{prefix, true}} {
+}
+
+void CommandPlugin::onMessage(IRCCommand cmd) {
+  m_parser.parse(cmd.params.back());
+  if (m_parser.commandsCount() > 1) {
+    LOG(WARNING, "Too many commands parsed: ", m_parser.commandsCount());
+  }
+
+  CommandParser::Command cmd = m_parser.getCommand();
+  cmd.additional_arguments = {
+    cmd.servername,
+    cmd.user,
+    cmd.nick,
+    cmd.host
+  };
+  cmd.additional_arguments.insert(
+      cmd.additional_arguments.end(),
+      cmd.params.begin(),
+      cmd.params.end() - 1
+  );
+
+  callCommand(cmd);
+}
+
+bool CommandPlugin::filter(const IRCCommand& cmd) {
+  return (cmd.command == "PRIVMSG");
+}
+
+void CommandPlugin::addCommand(const std::string& cmd, CmdFunction f) {
+  if (m_functions.count(cmd)) {
+    LOG(WARNING, "Overwriting function for command: ", cmd);
+  }
+
+  m_functions[cmd] = f;
+}
+
+void CommandPlugin::removeCommand(const std::string& cmd) {
+  size_t removed = m_functions.erase(cmd);
+  if (removed == 0) {
+    LOG(WARNING, "Such command (", cmd, ") didn't exist.");
+  }
+}
+
+void CommandPlugin::callCommand(const CommandParser::Command& command) {
+  if (m_functions.count(command.command)) {
+    m_functions[command.command](command);
+  } else {
+    LOG(ERROR, "Function for such command doesn't exist: ", command.command);
+  }
+}
