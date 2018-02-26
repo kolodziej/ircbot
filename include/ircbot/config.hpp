6c9b6889a08de5bac4df72b758502063f2e8be35
Kacper Kołodziej <kacper@kolodziej.it>
2018-02-27 00:02:33 +0100

    Add documentation for Config class

--- include/ircbot/config.hpp
+++ include/ircbot/config.hpp
@@ -6,22 +6,61 @@
 
 namespace pt = boost::property_tree;
 
+/** \class Config 
+ *
+ * \brief Proxy class for boost::property_tree::ptree
+ *
+ * This class plays a role of simple proxy for boost::property_tree::ptree
+ * object which keeps configuration loaded from json file. Config can handle
+ * configuration from already existing ptree or from saved file. It also allows
+ * to save modified tree back to file.
+ */
+
 class Config {
  public:
+  /** Defualt constructor - creates empty object */
   Config() = default;
+
+  /** Constructor loading configuration from file
+   *
+   * \param fname path to json configuration file
+   */
   Config(const std::string& fname);
+
+  /** Constructor handling existing ptree
+   *
+   * ptree object will be copied into Config instance.
+   *
+   * \param pt boost::property_tree:ptree object
+   */
   Config(const pt::ptree& pt);
 
+  /** Load configuration from file given in constructor */
   void loadFile();
+
+  /** Load configuration from given file
+   *
+   * \param fname path to json configuration file
+   */
   void loadFile(const std::string& fname);
 
+  /** Save configuration to file given in constructor */
   void saveFile();
+
+  /** Save configuration to given file
+   *
+   * \param fname path to json configuration file
+   */
   void saveFile(const std::string& fname);
 
+  /** Returns a reference to ptree object */
   pt::ptree& tree();
 
  private:
+  /** path of configuration file */
   std::string m_fname;
+
+  /** property tree handling configuration */
   pt::ptree m_pt;
 };
 
