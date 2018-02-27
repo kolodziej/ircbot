cd50aa1412fb29b69c7fadd5c424ecfa7f53a122
Kacper Kołodziej <kacper@kolodziej.it>
2018-02-27 17:42:23 +0100

    Add documentation for Plugin and PluginConfig

--- include/ircbot/plugin_config.hpp
+++ include/ircbot/plugin_config.hpp
@@ -8,9 +8,24 @@
 
 class Client;
 
+/** \class PluginConfig 
+ *
+ * \brief Plugin configuration for plugins loaded from shared object files.
+ *
+ * Keeps base data needed to load plugin from shared object:
+ *   - pointer to client instance,
+ *   - plugin's id
+ *   - plugin's configuration
+ */
+
 struct PluginConfig {
+  /** pointer to Client instance */
   std::shared_ptr<Client> client;
+
+  /** plugin id - unique in Client's scope */
   std::string id;
+
+  /** plugin configuration - actual configuration of plugin */
   Config config;
 };
 
