c3a0994e6969374fb5430ddc0037c2f52b1706de
Kacper Kołodziej <kacper@kolodziej.it>
2018-04-16 16:01:38 +0200

    Add draft of ircbotctl app

---
+++ program/ircbotctl.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+
+#ifndef SOCKET_PATH
+#define SOCKET_PATH "/var/run/ircbot.admin"
+#endif
+
+namespace asio = boost::asio;
+namespace opt = boost::program_options;
+
+int main(int argc, char** argv) {
+  std::string socket_fname;
+
+  opt::options_description opts("IRCBot CTL");
+  opts.add_options()
+    ("help,h", "show help message")
+    ("version,v", "show version information")
+    ("socket,s",
+     opt::value<std::string>(&socket_fname)->default_value(SOCKET_PATH),
+     "host on which ircbot admin port is listening")
+  ;
+  
+  opt::variables_map var;
+  try {
+    opt::store(opt::parse_command_line(argc, argv, opts), var);
+
+    if (var.count("help")) {
+      std::cout << opts;
+      return 1;
+    }
+
+    if (var.count("version")) {
+      std::cout << "ircbotctl\n";
+      return 0;
+    }
+
+    opt::notify(var);
+  } catch (opt::error& erc) {
+    std::cerr << "Problem during parsing arguments!\n";
+  }
+
+  std::cout << "Chosen admin port: " << socket_fname << '\n';
+
+  asio::io_service io;
+
+  asio::local::stream_protocol::endpoint ep(socket_fname);
+  asio::local::stream_protocol::socket socket(io);
+  boost::system::error_code ec;
+  socket.connect(ep, ec);
+  if (ec) {
+    std::cerr << "Could not connect to admin port unix socket!\n";
+    return 2;
+  }
+
+  std::cout << "Probably connected.. :-)\n";
+
+  bool quit = false;
+  const char* prompt = "ircbotctl> ";
+  while (not quit) {
+    char* line = readline(prompt);
+    size_t size = strlen(line);
+    if (size > 0) {
+      add_history(line);
+
+      if (strcmp(line, "quit") == 0) {
+        quit = true;
+      } else {
+        socket.send(asio::buffer(line, size));
+      }
+    }
+  }
+
+  socket.shutdown(
+    asio::local::stream_protocol::socket::shutdown_both
+  );
+  socket.close();
+
+  return 0;
+}
