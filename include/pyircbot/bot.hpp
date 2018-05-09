a324047051c943fc9466c5384d18ca84caf43fb1
Kacper Kołodziej <kacper@kolodziej.it>
2018-05-09 23:05:25 +0200

    Add connecting and sending methods to Bot

--- include/pyircbot/bot.hpp
+++ include/pyircbot/bot.hpp
@@ -2,8 +2,12 @@
 #define _PYIRCBOT_BOT_HPP
 
 #include <pybind11/pybind11.h>
+#include <boost/asio.hpp>
+
+#include <array>
 
 namespace py = pybind11;
+namespace asio = boost::asio;
 
 class Bot {
  public:
@@ -12,9 +16,20 @@ class Bot {
   std::string hostname() const;
   uint16_t port() const;
 
+  void connect();
+  void send(const std::string& data);
+
  private:
   const std::string m_hostname;
   const uint16_t m_port;
+
+  asio::io_service m_io;
+  asio::ip::tcp::socket m_socket;
+
+  std::array<char, 8192> m_buffer;
+
+  void receive();
+  void parse();
 };
 
 PYBIND11_MODULE(pyircbot, m) {
@@ -22,7 +37,9 @@ PYBIND11_MODULE(pyircbot, m) {
     .def(py::init<const std::string&, uint16_t>(),
          py::arg("hostname"), py::arg("port"))
     .def("hostname", &Bot::hostname)
-    .def("port", &Bot::port);
+    .def("port", &Bot::port)
+    .def("connect", &Bot::connect)
+    .def("send", &Bot::send);
 }
 
 #endif
--- pyircbot/bot.cpp
+++ pyircbot/bot.cpp
@@ -4,7 +4,8 @@
 
 Bot::Bot(const std::string& hostname, uint16_t port) :
   m_hostname{hostname},
-  m_port{port} {
+  m_port{port},
+  m_socket{m_io} {
 }
 
 std::string Bot::hostname() const {
@@ -13,4 +14,37 @@ std::string Bot::hostname() const {
 
 uint16_t Bot::port() const {
   return m_port;
+}
+
+void Bot::connect() {
+  asio::ip::tcp::resolver resolver{m_io};
+  auto endpoint_it = resolver.resolve({ m_hostname, std::to_string(m_port) });
+  auto endpoint = endpoint_it->endpoint();
+
+  m_socket.connect(endpoint);
+}
+
+void Bot::send(const std::string& data) {
+  m_socket.send(asio::buffer(data.data(), data.size()));
+}
+
+void Bot::receive() {
+  auto callback = [this](const boost::system::error_code& ec, std::size_t bytes) {
+    if (ec == 0) {
+      parse();
+    } else {
+      return;
+    }
+
+    receive();
+  };
+
+  m_socket.async_receive(
+    asio::buffer(m_buffer.data(), m_buffer.size()),
+    callback
+  );
+}
+
+void Bot::parse() {
+
 }
