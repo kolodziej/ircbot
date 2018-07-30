daac0bbfe025dedbc735a0561e96cf361bea5bff
Kacper Kołodziej <kacper@kolodziej.it>
2018-07-30 08:50:27 +0200

    Add header for network::Server

---
+++ include/ircbot/network/server.hpp
@@ -0,0 +1,18 @@
+#ifndef _IRCBOT_NETWORK_SERVER_HPP
+#define _IRCBOT_NETWORK_SERVER_HPP
+
+#include "ircbot/network/basic_server.hpp"
+#include "ircbot/network/client.hpp"
+
+#include <array>
+#include <boost/asio.hpp>
+
+namespace asio = boost::asio;
+
+namespace ircbot {
+namespace network {
+
+}  // namespace network
+}  // namespace ircbot
+
+#endif
