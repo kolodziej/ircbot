533a5a95d3d55eaf474f1447ac69334895d7b59a
Kacper Kołodziej <kacper@kolodziej.it>
2018-09-05 22:49:34 +0200

    Add Doxygen strings

--- include/ircbot/network/basic_client.hpp
+++ include/ircbot/network/basic_client.hpp
@@ -6,12 +6,29 @@
 namespace ircbot {
 namespace network {
 
+/** \class BasicClient
+ *
+ * Client abstraction - interface for all clients' types
+ */
 class BasicClient {
  public:
+  /** virtual destructor */
   virtual ~BasicClient() = default;
 
+  /** Connect to server */
   virtual void connect() = 0;
+
+  /** Send message to server
+   *
+   * \param buf data that should be sent
+   */
   virtual void send(const std::string& buf) = 0;
+
+  /** Start receiving messages
+   *
+   * Start accepting messages from socket. It may start asynchronous receiving
+   * and return or receive and process next message from queue.
+   */
   virtual void receive() = 0;
 };
 
